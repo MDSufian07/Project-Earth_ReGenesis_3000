6351ab60ea42a3f7852c53ffecf8ce38c63274d9
Sufian <sufianabu206@gmail.com>
2026-07-16 02:08:12 +0600

    Setup Gun position

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class AimController : MonoBehaviour
+{
+    public Rig aimRig;
+    public float speed = 8f;
+
+    void Update()
+    {
+        float targetWeight = (Input.GetMouseButton(1) || Input.GetMouseButton(0)) ? 1f : 0f;
+
+        aimRig.weight = Mathf.Lerp(aimRig.weight, targetWeight, Time.deltaTime * speed);
+    }
+}
